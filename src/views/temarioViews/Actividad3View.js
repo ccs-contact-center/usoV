834fddc509514f662872e5200671add1b5dff021
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-08-12 09:16:05 -0500

    Update Actividad3View.js

--- src/views/temarioViews/Actividad3View.js
+++ src/views/temarioViews/Actividad3View.js
@@ -10,6 +10,7 @@ import {
 
   Input,
 } from 'reactstrap'
+import swal from "sweetalert"
 import actividad1 from '../../assets/img/usoV/usoV2.png'
 import AunthService from "../../services/AuthService"
 import API_CCS from '../../services/API_CCS'
@@ -36,7 +37,7 @@ class Actividad3View extends Component {
       palabra7: '',
       palabra8: '',
       id_ccs: this.Aunth.getProfile().id_ccs,
-      form: "usoV",
+      form: "usoV-3",
 
     }
   }
@@ -49,10 +50,37 @@ class Actividad3View extends Component {
 
   async onSave(e) {
     try {
-      var respuesta = await API.guardaActividad(this.state)
-      alert('Se guardo actividad: 3, con id: ' + respuesta[0].id)
+      var respuesta = await API.guardaActividad(this.state);
+
+      swal({
+        title: "Status Actividad",
+        text: "Se guardo la actividad: 3, con id: " + respuesta[0].id,
+        icon: "success",
+        dangerMode: true,
+        button: {
+          text: "Aceptar",
+          value: true,
+          visible: true,
+          className: "btn btn-primary",
+          reset: true,
+        },
+      });
+      
+      // alert("Se guardo la actividad: 1, con id: " + respuesta[0].id);
     } catch (err) {
-      console.log('loggea si hay un error')
+      swal({
+        title: "Status Actividad",
+        text: "No se guardo la actividad: 3, Intenta de nuevo. ",
+        icon: "error",
+        dangerMode: true,
+        button: {
+          text: "Cerrar",
+          value: true,
+          visible: true,
+          className: "btn btn-primary ",
+        },
+      });
+      console.log("loggea si hay un error");
     }
   }
 
